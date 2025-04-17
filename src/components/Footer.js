0d0158bdf29054712918875e4215f2d4c6771632
MrSugar <mrsugar2@yandex.kz>
2025-04-17 13:43:51 +0500

    Обновил стили и доработал компоненты

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer style={{ backgroundColor: "#ffcc00", padding: "15px", marginTop: "30px", textAlign: "center" }}>
-      <p>© {new Date().getFullYear()} Скорая помощь. Все права защищены.</p>
+      <p>© {new Date().getFullYear()} КГП на ПХВ "Областная станция скорой медицинской помощи" УЗ ОА. Все права защищены.</p>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <h1 className="site-title">🚑 Скорая помощь</h1>
+      <h1 className="site-title">🚑 КГП на ПХВ "Областная станция скорой медицинской помощи" УЗ ОА</h1>
       <p className="site-subtitle">Мы рядом, когда это важно</p>
     </header>
   );
